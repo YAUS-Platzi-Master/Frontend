68520bd0102311875557a330641a11a01355a98e
gposso <gilberto.posso@gmail.com>
2020-09-13 21:33:02 -0500

    feat create and list shorturl

--- src/container/LoginUser.jsx
+++ src/container/LoginUser.jsx
@@ -1,70 +1,127 @@
-import React, { useState } from 'react';
-// import PropTypes from 'prop-types';
-// import { connect } from 'react-redux';
-// import { urlUser } from '../actions';
+import React, { useState, Component } from 'react';
+import axios from 'axios';
 import '../assets/styles/container/loginuser.scss';
 
-const LoginUser = (props) => {
-  const [form, setValues] = useState({
-    long_url: '',
-    custom_url: false,
-  });
+class LoginUser extends Component {
 
-  const updateInput = (event) => {
-    setValues({
-      ...form,
-      [event.target.name]: event.target.value,
-    });
-  };
-  const handleSubmit = (event) => {
+  constructor(props) {
+    super(props);
+    this.state = {
+      longUrl: '',
+      customName: '',
+      token: this.getCookie('token'),
+      shortUrls: [],
+    };
+    this.longUrlChange = this.longUrlChange.bind(this);
+    this.customNameChange = this.customNameChange.bind(this);
+    this.createURL = this.createURL.bind(this);
+    this.getShortUrls = this.getShortUrls.bind(this);
+  }
+
+  componentDidMount() {
+    this.getShortUrls();
+  }
+
+  longUrlChange(event) {
+    this.setState({ longUrl: event.target.value });
+  }
+
+  customNameChange(event) {
+    this.setState({ customName: event.target.value });
+  }
+
+  getCookie(name) {
+    const match = document.cookie.match(RegExp('(?:^|;\\s*)' + name.replace(/([.*+?\^$(){}|\[\]\/\\])/g, '\\$1') + '=([^;]*)'));
+    return match ? match[1] : null;
+  }
+
+  createURL(event) {
     event.preventDefault();
-    props.urlUser(form);
-  };
-  return (
-    <div className='home_url'>
-      <form className='creating_url' onSubmit={handleSubmit}>
-        <h3>Sólo ingresa tu URL y clickea Acortar</h3>
-        <input
-          name='long_url'
-          className='inputUrl'
-          type='text'
-          placeholder='url'
-          onChange={updateInput}
-        />
-        <h3>Nombre Personalizado</h3>
-        <input
-          name='custom_url'
-          className='inputUrl'
-          type='text'
-          placeholder='url'
-          onChange={updateInput}
-        />
-        <br />
-        <button type='submit' className='corte'><span className='btn-text'>Acortar</span></button>
-      </form>
-      <div className='container'>
-        <div className='row'>
-          <div className='col-sm'>
-            <h3>Cantidad de Hits</h3>
-          </div>
-          <div className='col-sm'>
-            <h3>Localizacion Geografica</h3>
-          </div>
-          <div className='col-sm'>
-            <h3>HTTP de referencia</h3>
+    const { token, longUrl, customName } = this.state;
+    axios({
+      url: 'https://yaus-api.herokuapp.com/api/1.0/register/new_url',
+      method: 'post',
+      headers: {
+        'Authorization': `Token ${token}`,
+      },
+      data: {
+        'long_url': longUrl,
+        'custom_url': false,
+        'short_url_custom': 'hola-mundo2',
+      },
+    })
+      .then(({ data }) => {
+        console.log(data);
+        this.getShortUrls();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  getShortUrls() {
+    const { token } = this.state;
+    axios({
+      url: 'https://yaus-api.herokuapp.com/api/1.0/set_of_urls/',
+      method: 'get',
+      headers: {
+        'Authorization': `Token ${token}`,
+      },
+    })
+      .then(({ data }) => {
+        this.setState({ shortUrls: data.data });
+        console.log(this.state.shortUrls);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  render() {
+    const { longUrl } = this.state;
+    const { customName } = this.state;
+    const { shortUrls } = this.state;
+
+    const items = shortUrls.map((item) => <a role='button' className='list-group-item list-group-item-action' key={item.id}>https://yaus.xyz/{item.short_url}</a>);
+
+    return (
+      <div className='home_url'>
+        <form className='creating_url' onSubmit={this.createURL}>
+          <h3 className='text-dark mt-2'>Create URL</h3>
+          <input
+            name='long_url'
+            className='form-control mt-4'
+            type='url'
+            placeholder='https://example.com/test'
+            value={longUrl}
+            onChange={this.longUrlChange}
+          />
+          <input
+            name='custom_url'
+            className='form-control mt-2'
+            type='text'
+            placeholder='Custom name'
+            value={customName}
+            onChange={this.customNameChange}
+          />
+          <br />
+          <button type='submit' className='btn btn-block btn-outline-secondary w-75 mx-auto mb-2'>CREATE</button>
+        </form>
+        <div className='container mt-4'>
+          <div className='row mt-3'>
+            <div className='col-4'>
+              <ul className='list-group'>
+                {items}
+              </ul>
+            </div>
+            <div className='col-8 border'>
+              <p className='text-secondary text-left'>Created: </p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
-
-// const mapDispatchToProps = {
-//   urlUser,
-// };
-
-// Home.propTypes = {
-//   urlUser: PropTypes.func,
-// };
+    );
+  }
+}
 
 export default LoginUser;
